0c7f40fc5848885425b6423f31a62cf7cf506ed3
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-02-21 10:25:06 -0900

    about-me-page-done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import "./About.css";
-import zimmerProfile from "../../../public/zimmerProfile.png"
+import zimmerProfile from "../../../public/zimmerProfile.png";
 const About = () => {
-
   const [activeCategory, setActiveCategory] = useState("frontend");
 
   const handleCategoryClick = (category) => {
@@ -29,15 +28,49 @@ const About = () => {
         </div>
       </section>
       <section id="middle-section-about">
-        <img id="profileLogo" src={zimmerProfile} alt="profilePicZimmer" />
+        <img id="profileLogo" draggable="false" src={zimmerProfile} alt="profilePicZimmer" />
       </section>
       <section id="right-section-about">
-        <div className="right-section-contents font3">FRONTEND</div>
-        <div className="right-section-contents font3">BACKEND</div>
-        <div className="right-section-contents font3">MISC</div>
-        <div className="frontend-contents">FRONTEND</div>
-        <div className="backend-contents">BACKEND</div>
-        <div className="misc-contents">MISC</div>
+        <div id="right-section-contents-wrapper">
+        <div className="right-section-contents ha font3" onClick={() => handleCategoryClick("frontend")}>FRONTEND</div>
+        <div className="right-section-contents font3" onClick={() => handleCategoryClick("backend")}>BACKEND</div>
+        <div className="right-section-contents  font3" onClick={() => handleCategoryClick("misc")}>MISC</div>
+        </div>
+        <div id="right-section-render-contents-wrapper">
+
+        {activeCategory === "frontend" && <div id="frontend-contents">
+            <div className="render-images-container">
+               <img className="frontend-images" draggable="false" src="/frontendImg/htmlLogo.png" alt="HtmlLogo"/>
+               <img className="frontend-images" draggable="false" src="/frontendImg/cssLogo.png" alt="CSSLogo" />
+               <img className="frontend-images" draggable="false" src="/frontendImg/jsLogo.png" alt="JSLogo" />
+               <img className="frontend-images" draggable="false" src="/frontendImg/reactLogo.png" alt="ReactLogo" />
+               <img className="frontend-images" draggable="false" src="/frontendImg/reduxLogo.png" alt="ReduxLogo" />
+               <img className="frontend-images" draggable="false" src="/frontendImg/styledComponentsLogo.png" alt="styledComponentsLogo" />
+               <img className="frontend-images" draggable="false" src="/frontendImg/tailwindLogo.png" alt="TailwindLogo" />
+            </div>
+          </div>}
+        {activeCategory === "backend" && <div id="backend-contents">
+        <div children="render-images-container">
+               <img className="backend-images" draggable="false" src="/backendImg/expressLogo.png" alt="ExpressLogo"/>
+               <img className="backend-images" draggable="false" src="/backendImg/nodejsLogo.png" alt="NodeJsLogo" />
+               <img className="backend-images" draggable="false" src="/backendImg/psqlLogo.png" alt="PSQLLogo" />
+            </div>
+          </div>}
+        {activeCategory === "misc" && <div id="misc-contents">
+        <div className="render-images-container">
+               <img className="misc-images" draggable="false" src="/miscImg/apisLogo.png" alt="apisLogo"/>
+               <img className="misc-images" draggable="false" src="/miscImg/bashLogo.png" alt="bashLogo" />
+               <img className="misc-images" draggable="false" src="/miscImg/excalidrawLogo.png" alt="excalidrawLogo" />
+               <img className="misc-images" draggable="false" src="/miscImg/figmaLogo.png" alt="figmaLogo" />
+               <img className="misc-images" draggable="false" src="/miscImg/githubLogo.png" alt="githubLogo" />
+               <img className="misc-images" draggable="false" src="/miscImg/gitLogo.png" alt="gitLogo" />
+               <img className="misc-images" draggable="false" src="/miscImg/npmLogo.png" alt="npmLogo" />
+               <img className="misc-images" draggable="false" src="/miscImg/postmanLogo.png" alt="postmanLogo" />
+               <img className="misc-images" draggable="false" src="/miscImg/viteLogo.jpg" alt="viteLogo" />
+               <img className="misc-images" draggable="false" src="/miscImg/webpackLogo.png" alt="webpackLogo" />
+            </div>
+          </div>}
+        </div>
       </section>
     </div>
   );
